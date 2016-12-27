9525273b3c41d4fcfc812195b67cc13f26e109fd
Ivan Caminal <ivancaminal72@gmail.com>
2016-12-27 20:47:17 +0100

    Inicio de WebSockets
    1-conexión cliente al servidor
    2-servidor asigna id al cliente
    3-servidor envia comida
    4-cliente envia su posición

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-/**
- * Created by Ivan on 16/11/2016.
- */
 var express = require('express');
 var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+function Player(){
+    this.position={x:0 ,y:0};
+    this.radio=20;
+}
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+var listFood=[];
+for(var i=0; i<30; i++){
+    listFood.push({x:getRandomArbitrary(0,1600), y:getRandomArbitrary(0,1200)});
+}
+
+var players=[];
+
+var socketIds = [];
+
+function findIndex(socketId){
+    for(var n=0; n<socketIds.length; n++){
+        if(socketId==socketIds[n]){
+            return n;
+        }
+    }
+}
 
 //Our js files
 app.use("/js", express.static('js'));
@@ -12,8 +36,34 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/game.html');
 });
 
+io.on('connection', function(socket){
+    console.log('a user connected');
+    var i = socketIds.push(socket.id)-1;
+    players.push(new Player()); //falta arreglar si un client es desconecta i un altre es conecta no faci push
+    console.log('user index: ' + i);
+    socket.emit('index',{clientIndex: i});
+    socket.emit('initFood', listFood);
+    socket.emit('players', players);
+    socket.on('player', function (player) {
+        var i = findIndex(socket.id);
+        players[i].position.x=player.x;
+        players[i].position.y=player.y;
+        players[i].radio=player.
+        console.log('x: '+ players[i].position.x + 'y: '+ players[i].position.y);
+    });
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+
+
+setTimeout(function() {
+    io.emit('players', players);
+}, 1000);//Phaser default frame rate 60fps = 16.6ms
+
+
 // Server start
-app.listen(3000, function(){
+http.listen(3000, function(){
     console.log('listening on *:3000');
 });
 
--- js/game.js
+++ js/game.js
@@ -1,30 +1,39 @@
-/**
- * Created by Ivan on 29/11/2016.
- */
+//Game
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'agar_game', { preload: preload, create: create, update: update, render: render });
-
-function preload() {
-
-    game.load.image('background_white','img/white.png');
-    game.load.image('background_black','img/black.png');
-
-}
-
 var playerStartRadius = 20;
-var foodRadius = 8;
+var foodRadius=8;
 var velocityPlayer = 200;
 var player;
 var cursors;
 var food;
+var listFood =[];
 var bmpFood;
 var score = 0;
 
+//Communication socket io
+var socket = io();
+var actionTime = 0;
+var clientIndex;
+socket.on('index', function (id) {
+    console.log("socket new index: " + id.clientIndex);
+    clientIndex=id.clientIndex;
+});
+socket.on('initFood', function (list) {
+    game.state.pause();
+    console.log(list[0]);
+    listFood=list;
+    createFood(); //Create the actual food that arrives from the server
+});
+
+
+function preload() {
+    game.load.image('background_white','img/white.png');
+    game.load.image('background_black','img/black.png');
+}
+
 function create() {
-
     game.add.tileSprite(0, 0, 1600, 1200, 'background_white');
-
     game.world.setBounds(0, 0, 1600, 1200);
-
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     /*
@@ -48,19 +57,19 @@ function create() {
     //Player
     function Player(start_x, start_y, color) {
         this.radio = playerStartRadius;
-        this.position = {x: start_x, y: start_y};
         this.color = color;
+        this.oldPosition = {x:start_x, y:start_y};
         this.bmpPlayer = game.add.bitmapData(2*this.radio,2*this.radio);
         this.bmpPlayer.ctx.fillStyle = this.color;
         this.bmpPlayer.ctx.beginPath();
         this.bmpPlayer.ctx.arc(this.radio,this.radio,this.radio,0,2*Math.PI);
         this.bmpPlayer.ctx.closePath();
         this.bmpPlayer.ctx.fill();
-        this.bola = game.add.sprite(this.position.x, this.position.y, this.bmpPlayer);
+        this.bola = game.add.sprite(start_x, start_y, this.bmpPlayer);
         game.physics.arcade.enable(this.bola);
         this.bola.body.setCircle(playerStartRadius);
         this.bola.body.collideWorldBounds = true;
-    };
+    }
 
     Player.prototype.setVelocityX =  function(x){
       this.bola.body.velocity.x = x;
@@ -84,24 +93,7 @@ function create() {
         this.bola.height = 2*this.radio;
         this.bola.key.update();
     };
-
     player = new Player(game.world.centerX,game.world.centerY,'#ff9999');
-    // Food
-    bmpFood = game.add.bitmapData(2*foodRadius,2*foodRadius);
-    bmpFood.ctx.fillStyle = '#fff242';
-    bmpFood.ctx.beginPath();
-    bmpFood.ctx.arc(foodRadius,foodRadius,foodRadius,0,2*Math.PI);
-    bmpFood.ctx.closePath();
-    bmpFood.ctx.fill();
-    //food = game.add.group(World,"food",false,true,Phaser.Physics.ARCADE);
-    food = game.add.group();
-    food.enableBody=true;
-    food.physicsBodyType = Phaser.Physics.ARCADE;
-    for(i=0; i<30; i++){
-        var particle = food.create(game.world.randomX, game.world.randomY, bmpFood);
-        particle.body.setCircle(foodRadius);
-    }
-
     cursors = game.input.keyboard.createCursorKeys();
 
     //  Notice that the sprite doesn't have any momentum at all,
@@ -109,10 +101,20 @@ function create() {
     //  0.1 is the amount of linear interpolation to use.
     //  The smaller the value, the smooth the camera (and the longer it takes to catch up)
     game.camera.follow(player.bola, Phaser.Camera.FOLLOW_LOCKON, 0.1, 0.1);
-
 }
 
+
 function update() {
+    if(game.time.now - actionTime > 1000) { //Check if position has changed every 1 second
+        if (player.oldPosition.x != player.bola.x || player.oldPosition.y != player.bola.y) {
+            console.log('position changed');
+            //socket.emit('position',{x:player.bola.x, y:player.bola.y, radio:player.radio});
+            actionTime = game.time.now;
+            player.oldPosition.x = player.bola.x;
+            player.oldPosition.y = player.bola.y;
+        }
+    }
+
 
     if(score > 30){
         game.state.add('level2', level2);
@@ -161,7 +163,7 @@ function eatFood (oldplayer, deadparticle) {
     // Removes the particle
     deadparticle.kill();
 
-    // Add random food particle
+    // Add random listFood particle
     var particle = food.create(game.world.randomX, game.world.randomY, bmpFood);
     particle.body.setCircle(foodRadius);
     radio+=1;
@@ -169,5 +171,25 @@ function eatFood (oldplayer, deadparticle) {
     player.setRadius(radio);
 
     //  Add and update the score
-    score += 10;
+    score = (radio-19)*10;
+}
+
+function createFood() {
+    // Food
+    console.log('creando food')
+    bmpFood = game.add.bitmapData(2*foodRadius,2*foodRadius);
+    bmpFood.ctx.fillStyle = '#fff242';
+    bmpFood.ctx.beginPath();
+    bmpFood.ctx.arc(foodRadius,foodRadius,foodRadius,0,2*Math.PI);
+    bmpFood.ctx.closePath();
+    bmpFood.ctx.fill();
+    //listFood = game.add.group(World,"listFood",false,true,Phaser.Physics.ARCADE);
+    food = game.add.group();
+    food.enableBody=true;
+    food.physicsBodyType = Phaser.Physics.ARCADE;
+    for(var i=0; i<listFood.length; i++){
+        var particle = food.create(listFood[i].x, listFood[i].y, bmpFood);
+        particle.body.setCircle(foodRadius);
+    }
+    game.state.resume();
 }
